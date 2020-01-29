e291f73818120727788a7e8ec048026dd0d76409
Avi Shapira <ashapira@microsoft.com>
2020-01-29 16:58:16 +0200

    PR1

--- test_Board.py
+++ test_Board.py
@@ -6,8 +6,8 @@ from Board import Maze
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.maze = Maze([[1,2],
-                           [3,4]])
+        self.maze = Maze([[1, 2],
+                          [3, 4]])
 
     def test_get_fault_board_index(self):
         self.assertEqual(self.maze.getPlaceLocation((10, 1)), -1)
@@ -18,7 +18,6 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.maze.getPlaceLocation((0, 1)), 3)
         self.assertEqual(self.maze.getPlaceLocation((1, 1)), 4)
         self.assertEqual(self.maze.getPlaceLocation((1, 0)), 2)
-
 
 
 if __name__ == '__main__':
